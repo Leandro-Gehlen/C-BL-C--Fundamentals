dbd4c2cb564841cd02c7a238080d61f68f7ebdab
Leandro Gehlen <legedeveloper@gmail.com>
2023-04-11 10:57:11 -0300

    StopWatch ready to use!

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -23,7 +23,32 @@ namespace StopWatch
 
       string data = Console.ReadLine().ToLower();
       char type = char.Parse(data.Substring(data.Length - 1, 1));
-      Console.WriteLine(type);
+      int time = int.Parse(data.Substring(0, data.Length - 1));
+      int multplier = 1;
+
+      if (type == 'm')
+        multplier = 60;
+
+      if (time == 0)
+        System.Environment.Exit(0);
+
+      PreStart(time * multplier);
+
+
+
+
+    }
+    static void PreStart(int time)
+    {
+      Console.Clear();
+      Console.WriteLine("Ready...");
+      Thread.Sleep(1000);
+      Console.WriteLine("Set...");
+      Thread.Sleep(1000);
+      Console.WriteLine("Go!");
+      Thread.Sleep(2500);
+
+      Start(time);
 
     }
     static void Start(int time)
@@ -42,6 +67,7 @@ namespace StopWatch
       Console.Clear();
       Console.WriteLine("StopWatch finalizado!");
       Thread.Sleep(2000);
+      Menu();
     }
   }
 
