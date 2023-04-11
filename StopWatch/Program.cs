d9654ef360b809333e89e7f797f164bfa3ea88cb
Leandro Gehlen <legedeveloper@gmail.com>
2023-04-11 10:23:34 -0300

    first part of counter created

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -1,1 +1,49 @@
-﻿
+﻿using System;
+using System.Threading;
+
+namespace StopWatch
+{
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      Menu();
+
+    }
+
+    static void Menu()
+    {
+      Console.Clear();
+      Console.WriteLine("S - Segundos => 10s = 10 segundos");
+      Console.WriteLine("M - Minutos => 1m = 1 minuto");
+      Console.WriteLine("0 - Sair");
+      Console.WriteLine("Quanto tempo deseja contar? ");
+
+      string data = Console.ReadLine().ToLower();
+      char type = char.Parse(data.Substring(data.Length - 1, 1));
+      Console.WriteLine(type);
+
+    }
+    static void Start(int time)
+    {
+
+      int currentTime = 0;
+
+      while (currentTime != time)
+      {
+        Console.Clear();
+        currentTime++;
+        Console.WriteLine(currentTime);
+        Thread.Sleep(1000);
+
+      }
+      Console.Clear();
+      Console.WriteLine("StopWatch finalizado!");
+      Thread.Sleep(2000);
+    }
+  }
+
+}
+
