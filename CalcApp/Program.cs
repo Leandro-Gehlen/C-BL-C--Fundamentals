9e81b27bd768fc7a2550ff9c77d7b29f4c50cc75
Leandro Gehlen <legedeveloper@gmail.com>
2023-04-14 15:58:38 -0300

    CalcApp successfuly created!

--- CalcApp/Program.cs
+++ CalcApp/Program.cs
@@ -8,8 +8,7 @@ namespace CalcApp
   {
     static void Main(string[] args)
     {
-      Subtracao();
-      Soma();
+      Menu();
 
 
     }
@@ -26,6 +25,8 @@ namespace CalcApp
 
       float resultado = v1 + v2;
       Console.WriteLine($"O resultado é: {resultado} ");
+      Console.ReadKey();
+      Menu();
     }
     static void Subtracao()
     {
@@ -41,11 +42,95 @@ namespace CalcApp
 
       float resultado = v1 - v2;
       Console.WriteLine($"O resultado é {resultado}");
+      Console.ReadKey();
+      Menu();
 
-      Thread.Sleep(3000);
     }
 
+    static void Divisao()
+    {
+      Console.Clear();
+
+      Console.WriteLine("Primeiro valor: ");
+      float v1 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("Segundo valor: ");
+      float v2 = float.Parse(Console.ReadLine());
+
+      float resultado = v1 / v2;
+
+      Console.WriteLine("");
+
+      Console.WriteLine($"O resultado é : {resultado}");
+      Console.ReadKey();
+      Menu();
+
+
+    }
+
+    static void Multiplicacao()
+    {
+      Console.Clear();
+      Console.WriteLine("Primeiro valor: ");
+      float v1 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("Segundo valor: ");
+      float v2 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("");
+
+      float resultado = v1 * v2;
+      Console.WriteLine($"Resultado é igual a : {resultado}");
+      Console.ReadKey();
+      Menu();
+    }
+
+    static void Menu()
+    {
+      Console.Clear();
+      Console.WriteLine("wait...");
+      Thread.Sleep(1000);
+      Console.WriteLine("Ready...");
+      Thread.Sleep(1000);
+      Console.WriteLine("Go!");
+      Thread.Sleep(1000);
+      Console.WriteLine("Escolha a opção desejada: ");
+      Console.WriteLine("1 - Soma");
+      Console.WriteLine("2 - Subtração");
+      Console.WriteLine("3 - Divisão");
+      Console.WriteLine("4 - Multiplicação");
+      Console.WriteLine("0 - Sair");
+      Console.WriteLine("-----------------");
+      short selection = short.Parse(Console.ReadLine());
+
+
+      switch (selection)
+      {
+        case 1:
+          Soma();
+          break;
+        case 2:
+          Subtracao();
+          break;
+        case 3:
+          Divisao();
+          break;
+        case 4:
+          Multiplicacao();
+          break;
+        case 0:
+          System.Environment.Exit(0);
+          break;
+        default:
+          Console.WriteLine("Opção inválida. Tente novamente.");
+          Thread.Sleep(2000);
+          Menu();
+          break;
+      }
+    }
   }
+
+
 
 
 
