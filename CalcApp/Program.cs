20648bf2ca3d070ea1d3fb77295a91eafd75be96
Leandro Gehlen <legedeveloper@gmail.com>
2023-04-14 12:25:50 -0300

    CalcApp modification

--- CalcApp/Program.cs
+++ CalcApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace CalcApp
 {
@@ -7,11 +8,50 @@ namespace CalcApp
   {
     static void Main(string[] args)
     {
+      Subtracao();
+      Soma();
+
+
+    }
+    static void Soma()
+    {
       Console.Clear();
       Console.WriteLine("Primeiro valor: ");
       float v1 = float.Parse(Console.ReadLine());
 
+      Console.WriteLine("Segundo valor: ");
+      float v2 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("");
+
+      float resultado = v1 + v2;
+      Console.WriteLine($"O resultado é: {resultado} ");
     }
+    static void Subtracao()
+    {
+      Console.Clear();
+
+      Console.WriteLine("Primeiro valor: ");
+      float v1 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("Segundo valor: ");
+      float v2 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("");
+
+      float resultado = v1 - v2;
+      Console.WriteLine($"O resultado é {resultado}");
+
+      Thread.Sleep(3000);
+    }
+
   }
+
+
+
 }
+
+
+
+
 
